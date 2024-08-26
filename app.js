c900167547ddd480b62c392d44e4425b744e68c0
Niranjan V <rathinamniranjan@gmail.com>
2024-08-26 15:18:48 +0530

    deployed the website and backend server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:1234/v1/chat/completions';
+const url = 'https://aa3c-2405-201-e001-c0d5-7963-6418-9ca3-2372.ngrok-free.app/v1/chat/completions';
 const btn = document.querySelector('.btn-submit');
 const promptInput = document.querySelector('.prompt');
 const responseDiv = document.querySelector('.chat-response');
